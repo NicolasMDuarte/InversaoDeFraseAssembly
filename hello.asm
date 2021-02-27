dd53e023685f975fddc8748646e028c3b2015dc5
Nícolas Maisonnette <nicolasmd9@hotmail.com>
2021-02-27 12:35:55 -0300

    update fim

--- hello.asm
+++ hello.asm
@@ -1,53 +1,42 @@
-; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
-    include \masm32\include\masm32rt.inc
-; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+ .386                   
+.model flat, stdcall
+.stack 4096                   
+option casemap :none                    
 
-    .data
-      var4   db  "Jonas",13,10,0
+;Dependencias
+include \masm32\include\windows.inc     
+include \masm32\macros\macros.asm       
+include \masm32\include\masm32.inc
+include \masm32\include\gdi32.inc
+include \masm32\include\user32.inc
+include \masm32\include\kernel32.inc
+includelib \masm32\lib\masm32.lib
+includelib \masm32\lib\gdi32.lib
+includelib \masm32\lib\user32.lib
+includelib \masm32\lib\kernel32.lib
 
-    .code
+.data ;Variaveis
+    pal       db      "Jonas", 0 ;Palavra que sera invertida
+    lenPal    dd      4          ;Length da palavra - 1 -> posicao
 
-start:
-   
-; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+.code ;Codigo
+main:
+    mov eax, offset pal  ;eax recebe a localizacao da palavra
+    mov ebx, 0           ;ebx recebe 0
+    mov ecx, lenPal      ;ecx recebe a length da palavra
 
-    call main
-    inkey
-    exit
+inversao: ;Inverte ate chegar na metade da palavra
+    mov dh, byte ptr[eax + ebx] ;dh recebe a letra eax + ebx
+    mov dl, byte ptr[eax + ecx] ;dl recebe a letra eax + ecx
 
-; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
+    mov byte ptr[eax + ebx], dl ;Subsitui a letra eax + ebx por dl
+    mov byte ptr[eax + ecx], dh ;Subsitui a letra eax + ecx por dh
 
-main proc
-
-    mov esi, offset var4 + 0
-    mov edi, offset var4 + 4
-    mov ebx, offset var4
-
-
-    mov al, [esi]
-    mov bl, [edi]
-    mov [edi], al
-    mov [esi], bl
-    inc esi
-    dec edi
-    mov al, [esi]
-    mov bl, [edi]
-    mov [edi], al
-    mov [esi], bl
-    inc esi
-    dec edi
-    mov al, [esi]
-    mov bl, [edi]
-    mov [edi], al
-    mov [esi], bl
-	
-	
-    print ebx
-
-    ret
-
-main endp
-
-; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
-
-end start
+    inc ebx ;ebx = ebx + 1
+    dec ecx ;ecx = ecx - 1
+    cmp ebx, 3 ;Se ebx < metade
+    jne inversao ;Menor -> recomeca, senao prossegue
+    
+    print eax ;Printa eax, a palavra
+    
+    end main ;Fim da main
