7fee31f3ba265d411c768d5048c8e7af64d712d6
Nícolas Maisonnette <nicolasmd9@hotmail.com>
2021-02-27 11:05:56 -0300

    update 2

--- hello.asm
+++ hello.asm
@@ -2,16 +2,7 @@
     include \masm32\include\masm32rt.inc
 ; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 
-comment * -----------------------------------------------------
-                     Build this console app with
-                  "MAKEIT.BAT" on the PROJECT menu.
-        ----------------------------------------------------- *
-
-    .data?
-      value  dd ?
-
     .data
-      item   dd 0
       var4   db  "Jonas",13,10,0
 
     .code
@@ -27,33 +18,28 @@ start:
 ; ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤
 
 main proc
-	
-    mov	ebx, offset var4	
 
-    ;add     ebx,4
-
-
+    mov esi, offset var4 + 0
+    mov edi, offset var4 + 4
     mov ebx, offset var4
-    mov eax, 1
-    mov ecx, 4
 
 
-    mov al, byte ptr [ebx+ecx]
-    mov ah, byte ptr [ebx+eax]
-    mov byte ptr [ebx+ecx], ah
-    mov byte ptr [ebx+eax], al
-    inc eax
-    dec ecx
-    mov al, byte ptr [ebx+ecx]
-    mov ah, byte ptr [ebx+eax]
-    mov byte ptr [ebx+ecx], ah
-    mov byte ptr [ebx+eax], al
-    inc eax
-    dec ecx
-    mov al, byte ptr [ebx+ecx]
-    mov ah, byte ptr [ebx+eax]
-    mov byte ptr [ebx+ecx], ah
-    mov byte ptr [ebx+eax], al
+    mov al, [esi]
+    mov bl, [edi]
+    mov [edi], al
+    mov [esi], bl
+    inc esi
+    dec edi
+    mov al, [esi]
+    mov bl, [edi]
+    mov [edi], al
+    mov [esi], bl
+    inc esi
+    dec edi
+    mov al, [esi]
+    mov bl, [edi]
+    mov [edi], al
+    mov [esi], bl
 	
 	
     print ebx
