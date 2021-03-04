9b7b589bd64082bd6e9ec18ef2e2fe1ab9581a3c
Nícolas Maisonnette <nicolasmd9@hotmail.com>
2021-03-04 12:54:59 -0300

    end 2

--- PrintHexa/console.asm
+++ PrintHexa/console.asm
@@ -21,41 +21,42 @@ includelib \masm32\lib\kernel32.lib
 .code ;Codigo
 main:
     
-    declaracao:
+    declaracao: ;declaracao das variaveis
         mov eax, 170
         mov ebx, 10
         lea ecx, pal
         jmp divisao
        
-    divisao:
-        xor edx, edx
+    divisao: ;divisao dos numeros
+        xor edx, edx ;zera
         div ebx
 
-        add edx, 48
-        push edx
+        add edx, 48 ;tabela ascii
+        push edx ;coloca na pilha
 
         cmp eax, 0
-        jne divisao
-
+        jne divisao ;se nao for 0 volta
+	
+	;preparando para escrever
         mov ebx, 0
         mov edx, " "
         
         jmp escrever
 
-    escrever:
-        pop eax
-        mov dword ptr[ecx + ebx], eax
+    escrever: ;monta e escreve a palavra na tela
+        pop eax ;pega da pilha
+        mov dword ptr[ecx + ebx], eax ;coloca o numero
         mov eax, 0
 
-        inc ebx
+        inc ebx ;proxima posicao
 
-        mov dword ptr[ecx + ebx], edx
+        mov dword ptr[ecx + ebx], edx ;coloca o espaco
 
         inc ebx
 
         cmp ebx, 6
-        jne escrever
+        jne escrever ;se nao eh 6 volta
 
-        print ecx
+        print ecx ;printa o resultado
     
 end main ;Fim da main
